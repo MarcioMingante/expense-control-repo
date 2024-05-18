fab430dd6120e081089e9ad6908222a8ecc36e23
Márcio Mingante <marciomvp17@gmail.com>
2024-05-18 11:33:51 -0300

    conclui req 2

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,5 +1,21 @@
+import { useSelector } from 'react-redux';
+import { RootReducer } from '../types/types';
+
 function Wallet() {
-  return <div>TrybeWallet</div>;
+  const { email } = useSelector((state: RootReducer) => state.user);
+
+  return (
+    <main>
+      <header>
+        {/* <img src="" alt="" /> */}
+
+        <h2 data-testid="total-field">Total de despesas: 0</h2>
+        <h2 data-testid="header-currency-field">BRL</h2>
+
+        <h2 data-testid="email-field">{email}</h2>
+      </header>
+    </main>
+  );
 }
 
 export default Wallet;
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,8 +1,14 @@
 import { composeWithDevTools } from '@redux-devtools/extension';
-import { legacy_createStore } from 'redux';
+import { applyMiddleware, legacy_createStore } from 'redux';
+import thunk from 'redux-thunk';
 import { rootReducer } from './reducers';
 
-const store = legacy_createStore(rootReducer, composeWithDevTools());
+const store = legacy_createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
 
 if (window.Cypress) {
   window.store = store;
